a74d1cd49e6ddf967a3beda17a2a5a3f5f123f75
asm-dev <salamanca.m@outlook.com>
2022-11-29 21:05:42 +0100

    explore some further component options

--- movies-project/src/app/app.module.ts
+++ movies-project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CharactersListComponent } from './components/characters-list/characters-list.component';
 import { CharactersItemComponent } from './components/characters-item/characters-item.component';
+import { CharacterDetailsComponent } from './components/character-details/character-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharactersListComponent,
-    CharactersItemComponent
+    CharactersItemComponent,
+    CharacterDetailsComponent
   ],
   imports: [
     BrowserModule
